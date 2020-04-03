8b88a9e7f66537aa45b106ae1e32de95b57c23f5
Ashwin Amit <ashwinamit_github@fastmail.com>
2020-04-03 09:32:17 -0700

    Handle collisions

--- game.js
+++ game.js
@@ -1,12 +1,13 @@
-const dx = 2,
-  dy = -2;
+const ballRadius = 10;
 
 function runGame() {
   let canvas = document.getElementById("myCanvas");
   let ctx = canvas.getContext("2d");
   let state = {
     x: canvas.width / 2,
-    y: canvas.height / 2
+    y: canvas.height / 2,
+    dx: 2,
+    dy: -2
   };
   let handler = () => draw(canvas, ctx, state);
   setInterval(handler, 10);
@@ -14,16 +15,23 @@ function runGame() {
 
 function draw(canvas, ctx, state) {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  let { x, y } = state;
+  let { x, y, dx, dy } = state;
   drawBall(ctx, x, y);
 
   state.x += dx;
   state.y += dy;
+
+  if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
+    state.dx = -dx;
+  }
+  if (y + dy > canvas.height - ballRadius || y + dy < ballRadius) {
+    state.dy = -dy;
+  }
 }
 
 function drawBall(ctx, x, y) {
   ctx.beginPath();
-  ctx.arc(x, y, 10, 0, Math.PI * 2);
+  ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
   ctx.fillStyle = "#0095DD";
   ctx.fill();
   ctx.closePath();
