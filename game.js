15db36c93ee1dc53debd793f25c47d4507d5167c
Ashwin Amit <ashwinamit_github@fastmail.com>
2020-04-03 18:16:03 -0700

    Draw bricks

--- game.js
+++ game.js
@@ -1,5 +1,15 @@
 const ballRadius = 10;
 const paddleDimensions = [75, 10];
+const brickData = {
+  rows: 3,
+  columns: 5,
+  width: 75,
+  height: 20,
+  // Between bricks
+  padding: 10,
+  offsetTop: 30,
+  offsetLeft: 30
+};
 
 function runGame() {
   let canvas = document.getElementById("myCanvas");
@@ -11,6 +21,14 @@ function runGame() {
     }
     alert("Game Over");
   };
+
+  let bricks = [];
+  for (let col = 0; col < brickData.columns; col++) {
+    bricks[col] = [];
+    for (let row = 0; row < brickData.rows; row++) {
+      bricks[col][row] = { x: 0, y: 0 };
+    }
+  }
 
   let state = {
     ball: {
@@ -26,6 +44,7 @@ function runGame() {
       leftPressed: false,
       rightPressed: false
     },
+    bricks,
     gameOverHandler
   };
 
@@ -50,6 +69,7 @@ function runGame() {
 
 function draw(canvas, ctx, state) {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
+  drawBricks(ctx, state.bricks);
   drawBall(ctx, state.ball);
   drawPaddle(canvas, ctx, state.paddle);
   applyPhysics(canvas, state);
@@ -73,6 +93,31 @@ function drawPaddle(canvas, ctx, paddle) {
   ctx.closePath();
 }
 
+function drawBricks(ctx, bricks) {
+  let {
+    rows,
+    columns,
+    width,
+    height,
+    padding,
+    offsetLeft,
+    offsetTop
+  } = brickData;
+  for (let c = 0; c < columns; c++) {
+    for (let r = 0; r < rows; r++) {
+      let brickX = c * (width + padding) + offsetLeft;
+      let brickY = r * (height + padding) + offsetTop;
+      bricks[c][r].x = brickX;
+      bricks[c][r].y = brickY;
+      ctx.beginPath();
+      ctx.rect(brickX, brickY, width, height);
+      ctx.fillStyle = "#0095DD";
+      ctx.fill();
+      ctx.closePath();
+    }
+  }
+}
+
 function applyPhysics(canvas, state) {
   let ballState = state.ball;
   let { x, y, dx, dy } = ballState;
@@ -86,7 +131,7 @@ function applyPhysics(canvas, state) {
   if (y + dy < ballRadius) {
     ballState.dy *= -1;
   } else if (y + dy > canvas.height - ballRadius) {
-    // FIXME: we're ignoring paddle height here.
+    // FIXME: we're ignoring paddle here.
     if (x < state.paddle.x + paddleDimensions[0] && x > state.paddle.x) {
       ballState.dy *= -1;
     } else {
